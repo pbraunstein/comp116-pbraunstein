7960d775733f362498629dfbc2d57ad05f46ae86
pbraunstein <pbraunstein12@gmail.com>
2015-12-11 10:09:04 -0500

    Added scripts and wordlists directories to final submission

---
+++ finalproject/scripts/charFracSim.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#
+# charFracSim.py
+# Philip Braunstein
+# COMP116: Making Secure Easy-to-Remember Passwords
+#
+# Determines a perent similarity metric of character count similarity. Where
+# the percentage of each character of a string in the other string is
+# calculated.
+#
+
+from sys import exit
+from sys import argv
+
+def main():
+    if len(argv) != 3:
+        usage()
+
+    dicts1 = makeDict(argv[1])
+    dicts2 = makeDict(argv[2])
+
+    charPercList = compareDicts(dicts1, dicts2)
+
+    charPerc = float(sum(charPercList)) / float(len(charPercList))
+
+    print "Character Percent Similarity:", round(charPerc, 2)
+
+    exit(0)
+
+
+# Prints the usage of the script then exits nonzero
+def usage():
+    print "USAGE:", argv[0], "STRING_1 STRING_2"
+    exit(1)
+
+
+# Makes a dictionary of character counts from string s and returns this dict
+def makeDict(s):
+    toReturn = {}
+
+    for c in s:
+        try:
+            toReturn[c] += 1
+        except KeyError:
+            toReturn[c] = 1
+
+    return toReturn
+
+
+# Constructs a list of the percentage of each charater from one string in the
+# other. To get this number the smaller is divided into the bigger. For
+# example, if one string has 3 of one character and the other has 4 of that
+# character, the percentage is always 0.75 rather than 1.33. Returns this list.
+def compareDicts(dicts1, dicts2):
+    toReturn = []
+    allKeys = set(dicts1.keys() + dicts2.keys())  # Set for no duplicate keys
+
+    for key in allKeys:
+        try:
+            toReturn.append(float(min(dicts1[key], dicts2[key])) /\
+                                        float(max(dicts1[key], dicts2[key])))
+        except KeyError:
+            toReturn.append(0.0)  # If letter not found in a string, perc is 0
+
+    return toReturn
+
+
+if __name__ == '__main__':
+    main()
---
+++ finalproject/scripts/smartFracSim.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#
+# smartFracSim.py
+# Philip Braunstein
+# COMP116: Making Secure Easy-to-Remember Passwords
+#
+# Evaluates the percent similarity of two strings passed to the script on the
+# command line by averaging the forward and backgward percent similarities
+#
+
+from sys import exit
+from sys import argv
+
+def main():
+    if len(argv) != 3:
+        usage()
+
+    forPercSim = percSim(argv[1], argv[2])
+    revPercSim = percSim(argv[1][::-1], argv[2][::-1])
+
+
+    avgPercSim = (forPercSim + revPercSim) / 2.0
+
+    print "Forward Percent Similarity:", round(forPercSim, 2)
+    print "Reverse Percent Similarity:", round(revPercSim, 2)
+    print "Average Percent Similarity:", round(avgPercSim, 2)
+    
+    exit(0)
+
+
+# Prints the usage of the script then exits nonzero
+def usage():
+    print "USAGE:", argv[0], "STRING_1 STRING_2"
+    exit(1)
+
+
+# Returns the percent similarity of the strings s1 and s2
+# If the strings are of unequal lengths, spaces are inserted at the end of the
+# shorter string
+def percSim(s1, s2):
+    numSims = 0
+
+    if len(s1) < len(s2):
+        s1 = addNSpaces(s1, len(s2) - len(s1))
+    elif len(s2) < len(s1):
+        s2 = addNSpaces(s2, len(s1) - len(s2))
+
+    # INVARIANT: Both strings must be the same length at this point
+    for i in range(len(s1)):
+        if s1[i] == s2[i]:
+            numSims += 1
+
+
+    return float(numSims) / float(len(s1))
+
+
+
+
+
+# Returns a string s with spacesToAdd spaces appended to the end of it
+def addNSpaces(s, spacesToAdd):
+    for i in range(spacesToAdd):
+        s += ' '
+
+    return s
+
+
+if __name__ == '__main__':
+    main()
